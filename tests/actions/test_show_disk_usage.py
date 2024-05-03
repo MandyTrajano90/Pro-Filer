eb1da0a73db840d31171fac1ebb872a44f7ef05a
Mandytrajano90 <mandytrajano90@gmail.com>
2024-05-03 09:45:12 -0300

    Req 5 - disk_usage tests

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,15 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+
+
+def test_show_disk_usage(tmp_path, capsys):
+    text_file_1 = tmp_path / "test_file_1.txt"
+    text_file_1.write_text("This is the first test file")
+    text_file_2 = tmp_path / "test_file_2.txt"
+    text_file_2.write_text("This is the second test file")
+
+    context = {"all_files": [str(text_file_1), str(text_file_2)]}
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+    assert captured.out.replace(" ", "").endswith("Totalsize:55\n")
+    assert captured.out.replace(" ", "").split("\n")[0].endswith("28(50%)") 
+    assert captured.out.replace(" ", "").split("\n")[1].endswith("27(49%)")
